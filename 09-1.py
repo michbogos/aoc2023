9ebf8c8cf4f500061bbed714b63520cd8c401fa2
michbogos <bogostamicheal@gmail.com>
2023-12-09 08:48:22 +0100

    Day 9

---
+++ 09-1.py
@@ -0,0 +1,18 @@
+ress = 0
+
+with open("09.txt", 'r') as f:
+    for line in f:
+        nums = [int(i) for i in line.split(" ")]
+        arrays = [nums]
+        run = True
+        while run:
+            arrays.append([])
+            for i in range(len(arrays[-2])-1):
+                arrays[-1].append(arrays[-2][i+1]-arrays[-2][i])
+            if len(set(arrays[-1])) == 1 and arrays[-1][-1]==0:
+                run = False
+                arrays[-1].append(0)
+                for i in range(2, len(arrays)+1):
+                    arrays[-i].append(arrays[-i][-1]+arrays[-(i-1)][-1])
+        ress += arrays[0][-1]
+print(ress)
